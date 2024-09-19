4c6528b90cedbd624777c8ca2733dd37840f0155
Paul Shivers <48158453+PShivers@users.noreply.github.com>
2024-09-18 22:29:43 -0400

    reverted rules.mk

--- qmk_firmware/keyboards/keyball/keyball44/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball44/rules.mk
@@ -1,10 +1,8 @@
-CONVERT_TO=promicro_rp2040
-
 # MCU name
-MCU = RP2040
+MCU = atmega32u4
 
-# Bootloader selection for RP2040
-BOOTLOADER = rp2040
+# Bootloader selection
+BOOTLOADER = caterina
 
 # Link Time Optimization required for size.
 LTO_ENABLE = yes
@@ -18,28 +16,28 @@ NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
 
-# Keyball44 is split keyboard.
+# Keyball39 is split keyboard.
 SPLIT_KEYBOARD = yes
 
 # Optical sensor driver for trackball.
 POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = custom
 SRC += drivers/pmw3360/pmw3360.c
-QUANTUM_LIB_SRC += spi_master.c # Optical sensor uses SPI to communicate
+QUANTUM_LIB_SRC += spi_master.c # Optical sensor use SPI to communicate
 
 # This is unnecessary for processing KC_MS_BTN*.
 MOUSEKEY_ENABLE = no
 
 # Enabled only one of RGBLIGHT and RGB_MATRIX if necessary.
 RGBLIGHT_ENABLE = no        # Enable RGBLIGHT
-RGB_MATRIX_ENABLE = no      # Enable RGB_MATRIX (not working yet)
+RGB_MATRIX_ENABLE = no      # Enable RGB_MATRIX (not work yet)
 RGB_MATRIX_DRIVER = ws2812
 
-# Do not enable SLEEP_LED_ENABLE. It uses the same timer as BACKLIGHT_ENABLE
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 
 # To support OLED
-OLED_ENABLE = no                # Please enable this in each keymap.
+OLED_ENABLE = no                # Please Enable this in each keymaps.
 SRC += lib/oledkit/oledkit.c    # OLED utility for Keyball series.
 
 # Include common library
@@ -49,4 +47,3 @@ SRC += lib/keyball/keyball.c
 SPACE_CADET_ENABLE = no
 GRAVE_ESC_ENABLE = no
 MAGIC_ENABLE = no
-
